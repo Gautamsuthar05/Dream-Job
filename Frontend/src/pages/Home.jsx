8620b879a4b134a5d8be8fc696768d673164c0ad
Gautam Suthar <gbsutharr185@gmail.com>
2025-10-04 20:25:22 +0530

    add footer

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Jobs from "../components/Jobs";
 import Navbar from "../components/Navbar";
-
+import Footer from "../components/Footer";
 function Home() {
   return (
     <>
       <Navbar />
-      <Jobs/>
+      <Jobs />
+      <Footer />
     </>
   );
 }
