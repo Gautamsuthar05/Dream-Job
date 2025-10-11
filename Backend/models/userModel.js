2bf2f36eb99fa25eb8fb71d3e8bd7fd31810e672
Gautam Suthar <gbsutharr185@gmail.com>
2025-10-11 21:43:17 +0530

    manage backend

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, min: 8, max: 16, required: true },
-  role: { type: String, enum: ["user", "admin"], default: "user" },
+  role: { type: String, enum: ["user", "recruiter"], default: "user" },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
   isAccountVerified: { type: Boolean, default: false },
